0f7e35c22c37606dcf84997cc2c87a8883f5aa76
Maksym Stoianov <stoianov.maksym@gmail.com>
2025-01-30 12:56:10 +0100

    add components/ErrorMessage

---
+++ src/components/ErrorMessage/styles.ts
@@ -0,0 +1,12 @@
+import styled from "@emotion/styled";
+
+export const ErrorMessage = styled.div`
+	width: 600px;
+	padding: 20px;
+	background-color: #eb9378;
+	border: 2px solid #e61212;
+	border-radius: 6px;
+	color: #e61212;
+	font-size: 28px;
+	font-weight: bold;
+`;
